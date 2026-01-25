a884d3d4c03ec01f769cca2b662b6d77be25939a
Nishuy52 <jyuc1109sch@gmail.com>
2026-01-25 20:15:10 +0800

    Renamed Duke to Pippy

---
+++ src/main/java/Pippy.java
@@ -0,0 +1,15 @@
+public class Pippy {
+    static void main(String[] args) {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        String hello = " Hello! I'm Pippy\n" +
+                " What can I do for you?\n";
+        String bye =  " Bye. Hope to see you again soon!\n";
+        String line = "____________________________________________________________\n";
+
+        System.out.println(line + hello + line + bye + line);
+    }
+}
